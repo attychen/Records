9e4c960a9997c207d795ffae0988fb3195d15d19
attychen <102278257+attychen@users.noreply.github.com>
2022-03-29 13:50:15 +0800

    let

--- Front-end/es6/块作用域/let.js
+++ Front-end/es6/块作用域/let.js
@@ -4,6 +4,7 @@
 // es5：全局作用域 函数作用域
 // es6: 增加块级作用域
 
+
 /*if(true){
 
     let a=123;
@@ -19,3 +20,5 @@ console.log(a);
 
 
 // 打印结果 let.js:18 Uncaught ReferenceError: a is not defined
+
+//  使用 let 定一个变量 是把这个变量绑定在声明它的地方 在 { } 外面 是访问不到的
